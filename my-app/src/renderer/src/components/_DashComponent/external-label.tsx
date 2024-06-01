d89b7f9f2781ef25a532bc1457fdf3cca5177232
Suhaib Rawajbeh <149022910+ItsLawy@users.noreply.github.com>
2024-06-01 15:41:53 +0300

    Delete my-app directory

--- my-app/src/renderer/src/components/_DashComponent/external-label.tsx
+++
@@ -1,8 +0,0 @@
-
-
-
-export default function External({isStatus}): JSX.Element {
-        return (
-            <label className="w-[8vw] h-[3vh] text-lg text-center m-auto font-medium text-[14px]"style={{ color: isStatus === 'Paid' ? '#96CB9C' : (isStatus === 'Pending' ? '#febf19' : '#FF6347')}}>{isStatus}</label>
-        )
-}
--- my-app/src/renderer/src/components/_OrderComponents/External.tsx
+++
@@ -1,7 +0,0 @@
-
-
-export default function External({isStatus}): JSX.Element {
-    return (
-        <label className="w-[4vw] h-[3vh] text-lg text-center m-auto font-medium text-[14px]"style={{ color: isStatus === 'Paid' ? '#96CB9C' : (isStatus === 'Pending' ? '#febf19' : '#FF6347')}}>{isStatus}</label>
-    )
-}
